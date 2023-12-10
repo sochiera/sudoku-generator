5713f60785fc040c518c3c4995bd7829c4398af3
Jan Sochiera <jan@sochiera.pl>
2023-12-10 09:14:20 +0100

    Extracted Sudoku class from SudokuGenerator

--- sudoku_generator.py
+++ sudoku_generator.py
@@ -2,11 +2,9 @@ from sudoku_validate import is_sudoku_correct
 import numpy as np
 
 class Sudoku:
-    def __init__(self, n):
+    def __init__(self, rows):
         # what about using numpy array/matrix here too?
-        self._rows = []
-        for i in range(n):
-            self._rows += [[(j+i)%n +1 for j in range(n)]]
+        self._rows = rows
 
     def get_rows(self):
         return self._rows
@@ -26,10 +24,17 @@ class Sudoku:
     def __str__(self):
         return str(self.get_rows())
 
+class SudokuGenerator:
+    def __init__(self, n):
+        self._n = n
+
+    def generate(self):
+        n = self._n
+        rows = []
+        for i in range(n):
+            rows += [[(j+i)%n +1 for j in range(n)]]
+        return Sudoku(rows)
 
 def generate_sudoku(n):
-    return Sudoku(n)
-
-if __name__ == '__main__':
-    s = Sudoku(2)
-    s.print()
+    sg = SudokuGenerator(n)
+    return sg.generate()
